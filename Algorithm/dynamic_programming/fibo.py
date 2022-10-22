636f13096a947aee08f0ead1ffa74e74b36df289
inan <ghjklla007@naver.com>
2022-10-22 22:07:02 +0900

    피보나치

--- Algorithm/dynamic_programming/fibo.py
+++ Algorithm/dynamic_programming/fibo.py
@@ -9,6 +9,9 @@ def fib_naive(n):
 
 # top-down
 # recursive, stack 제한이 있음
+def fib_recur_dp(n):
+    pass
+
 
 # bottom-up
 def fib_dp(n):
@@ -19,6 +22,11 @@ def fib_dp(n):
 
     fib_array = [0, 1]
 
+    for i in range(2, n+1):
+        num = fib_array[i-1] + fib_array[i-2]
+        fib_array.append(num)
+    return fib_array[n]
 
 if __name__ == '__main__':
-    print(fib_naive(35))
+    # print(fib_naive(35))
+    print(fib_dp(35))
