72fcc6b9990d03a898b31228e32e51da4af06b33
inhwanJeong <ghjklla007@naver.com>
2022-10-26 23:13:50 +0900

    알고리즘 코드 수정

--- Algorithm/dynamic_programming/make_one.py
+++ Algorithm/dynamic_programming/make_one.py
@@ -1,19 +1,21 @@
 if __name__ == '__main__':
-    max = 1000000
+    max = 1000000+1
     memo = [0 for i in range(max)]
 
     target = int(input())
 
-    for i in range(2, max):
+
+    for i in range(2, target+1):
+        temp = []
         if i % 3 == 0:
-            memo[i] = memo[int(i/3)] + 1
-            continue
+            temp.append(memo[int(i/3)] + 1)
 
         if i % 2 == 0:
-            memo[i] = memo[int(i/2)] + 1
-            continue
+            temp.append(memo[int(i/2)] + 1)
 
-        memo[i] = memo[i-1] + 1
+        temp.append(memo[i-1] + 1)
+        print(temp)
+        memo[i] = min(temp)
 
     print(memo[target])
 
